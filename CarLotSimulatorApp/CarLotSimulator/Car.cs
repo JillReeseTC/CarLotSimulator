ed2ca6828b0f75fef35942488deaca4c0931b9f6
Jill Reese <ajreese@gmail.com>
2023-01-09 13:13:37 -0500

    Partial Answer Exercise 2 - car class created

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+    //Default Constructor
+    public Car()
+    {
+    }
+        //Properties for Car
+        public int? Year { get; set; }
+        public string? Make { get; set; } 
+        public string? Model { get; set; }
+        public string? EngineNoise { get; set }
+        public string? HonkNoise { get; set; }
+        public bool? IsDriveable { get; set; }
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    }
+}
